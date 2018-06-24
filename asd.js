2e1756b17324894664d538f26d9b48ff05d6aa96
wilbi <wilbi@hotmail.fi>
2018-06-24 18:24:08 -0500

    Refactoring, changes to output, random team maker
    Removed accumulated unused variables, changed how the random teams algorithm is implemented for better performance. Added new information to the team display screen. Made the code overall more readable

--- asd.js
+++ asd.js
@@ -2,87 +2,100 @@ $(document).ready(function(){
 /*This method is better for small datasets*/
 	$("#send").on("click", function(){
 		//sanity checks on user input
-		let teamsize = parseInt($("#size").val());
-		if (isNaN(teamsize) || teamsize <= 0) return -1;
-		let names = $("#names").val();
-		let sr = $("#sr").val();
-		names = parseInput(names);
-		sr = parseInput(sr);
-		sr = sr.map(a => parseInt(a));
-		if(isNaN(sr[sr.length-1])) sr.pop();
-		if(names.length != sr.length) names.pop();
-		if(names.length === 0 || sr.length === 0) return -1;
+		$("#send").attr("disabled", "disabled")
+		/*
+		Fix team sizes not being respected
+		*/
+
+		let data = readData();
+		let names = data.names;
+		let sr = data.sr;
+		const teamsize = data.teamsize;
+
+		/* Add messages to user */
+
 		const pool = sr.length;
-		let best = 999999, bestTeam, exclude, bestExclude, teamSR, i, r = 0, choice, temparr, diff = 400, average, j, teams = [], arr = [];
-		while(best >= 100*teamsize && r != 100000){
-			for(let h = 0; h < Math.floor(pool/teamsize)+1; h++){
+		const poolSize = Math.floor(pool/teamsize);
+		let best = 999999, bestTeam, exclude = [], i, r = 100000, choice, average, teams = [], teamNum = [];
+		/* Merge the lists */
+		let arr = [];
+		for(let i = 0; i < pool; i++){
+			let name = names.pop();
+			let rank = sr.pop();
+			arr.push({"name":name, "rank":rank});
+		}
+		//console.log(arr)
+		for(let i = 0; i<poolSize; i++){
+			teamNum.push(i);
+		}
+		/* There has to be a better way than doing over n*100000 iterations to Generate
+		pseudo balanced pseudo random teams */
+		let dup;
+		console.log("start loop")
+		while(best >= 50 && r--){
+			for(let h = 0; h < poolSize; h++){
 				teams.push([]);
 			}
-			average=[];
-			temparr = sr.slice();
-			i = 0;
-			choice = 0;
-			j = 1
-			while(temparr.length != 0){
-				choice = Math.floor(Math.random()*temparr.length);
-				teams[i].push(temparr[choice]);
-				temparr.splice(choice, 1);
-				if(j === teamsize){ i++;j = 0;}
-				j++;
+			exclude = [];
+			while((arr.length - exclude.length)%teamsize != 0){
+				exclude.push(Math.floor(Math.random()*arr.length))
+				exclude = [... new Set(exclude)];
 			}
-			if (teams[i].length != teamsize) exclude = teams.pop();
-			average = teams.map(team => team.reduce(function(sum, val){return sum+val;},0));
+			average=[];
+			dup = teamNum.slice();
+			i = arr.length;
+			while(i--){
+			if(exclude.filter(index => index === i).length != 0) continue;
+				choice = Math.floor(Math.random()*dup.length);
+				teams[dup[choice]].push(arr[i]);
+				if(teams[dup[choice]].length === teamsize) dup.splice(choice,1);
+			}
+			average = teams.map(team => {
+				let tAvg = team.reduce(function(sum, player){return sum+player.rank;},0)/teamsize
+				team.avgSr = tAvg;
+				return tAvg;
+			});
 			diff = average.reduce((a,b) => Math.max(a,b)) - average.reduce((a,b) => Math.min(a,b));
 			if (diff < best) {
 				bestTeam = teams.slice();
 				best = diff;
 				bestExclude = exclude;
-				teamSR = average
 			}
-			r++;
 			teams = [];
 		}
-		console.log("loop done", r)
-		//Generate JSON out of the data because json is nice
-		generatedTeams = bestTeam.map(team => team.map(player => {
-			let index = sr.indexOf(player);
-			let name = names[sr.indexOf(player)];
-			sr.splice(index, 1);
-			names.splice(index,1);
-			return {"name":name, "sr": player}
-		}));
+		console.log("Stop loop", r)
+		//console.log(bestTeam, teams, dup, teamNum, exclude, arr[exclude[0]])
 		//Formulate the eventual output
-		let teamOutput = "Average Difference: " + Math.floor(best/teamsize) + " Teams:\n";
-		for(i = 0; i < generatedTeams.length; i++){
-			teamOutput += "Team " + (i+1) + " Average Sr: " + Math.floor(teamSR[i]/teamsize);
-			for(j = 0; j < generatedTeams[i].length; j++){
-				teamOutput += "\n" + generatedTeams[i][j].name + ", " + generatedTeams[i][j].sr + " sr";
+		let teamOutput = "Largest Difference: " + Math.floor(best)
+		teamOutput += "\nAverage Sr: " + Math.floor(bestTeam.reduce((total, team) => team.avgSr+total, 0)/bestTeam.length)
+		teamOutput += "\nTeams:\n";
+		teamOutput += (exclude.length === 0) ? "" : "Excluded: " + exclude.map(index => arr[index].name).join(", ") + "\n"
+		for(i = 0; i < bestTeam.length; i++){
+			teamOutput += "Team " + (i+1)+ ","+ bestTeam[i].length + " Average Sr: " + Math.floor(bestTeam[i].avgSr);
+			for(let j = 0; j < bestTeam[i].length; j++){
+				teamOutput += "\n" + bestTeam[i][j].name + ", " + bestTeam[i][j].rank + " sr";
 			}
 			teamOutput += "\n"
 		}
-
 		$("#teamDisplay").html(teamOutput);
+		$("#send").removeAttr("disabled")
 	});
+
+/* Generate test data */
 
 	$("#testData").on("click", () => {
 		let names = [];
 		let srs = [];
-		for(let i = 0; i < 500; i++){
+		for(let i = 0; i < 75; i++){
 			names.push(Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0,7));
 			srs.push((Math.floor(norm()*2500)+2500));
 		}
-		$("#names").append(names.map((name) => { return name + '\n'}));
-		$("#sr").append(srs.map((sr) => {return sr + '\n'}));
+		$("#names").html(names.map((name) => { return name + '\n'}));
+		$("#sr").html(srs.map((sr) => {return sr + '\n'}));
 	})
-	function parseInput(input){
-		parser = input.replace(/(\s)+/gim, ";")
-		return parser.split(";");
-	}
-	let norm = () => {
-		return ((Math.random() + Math.random() + Math.random() + Math.random() + Math.random() + Math.random()) - 3) / 3;
-	}
-/*This method is better for large datasets*/
-	$("#alg").on("click", () => {
+
+/* Parse Inputs */
+	function readData(){
 		let teamsize = parseInt($("#size").val());
 		if (isNaN(teamsize) || teamsize <= 0) return -1;
 		let names = $("#names").val();
@@ -92,12 +105,51 @@ $(document).ready(function(){
 		sr = sr.map(a => parseInt(a));
 		if(isNaN(sr[sr.length-1])) sr.pop();
 		if(names.length != sr.length) names.pop();
-		/* Add messages to user */
 		if(names.length === 0 || sr.length === 0) return -1;
+
+		return {names, sr, teamsize}
+
+	}
+
+	function parseInput(input){
+		parser = input.replace(/(\s)+/gim, ";")
+		return parser.split(";");
+	}
+/* Generate normally distributed numbers */
+	let norm = () => {
+		return ((Math.random() + Math.random() + Math.random() + Math.random() + Math.random() + Math.random()) - 3) / 3;
+	}
+
+/*
+	This method is better for large datasets
+*/
+	$("#alg").on("click", () => {
+		/*
+		let teamsize = parseInt($("#size").val());
+		if (isNaN(teamsize) || teamsize <= 0) return -1;
+		let names = $("#names").val();
+		let sr = $("#sr").val();
+		names = parseInput(names);
+		sr = parseInput(sr);
+		sr = sr.map(a => parseInt(a));
+		if(isNaN(sr[sr.length-1])) sr.pop();
+		if(names.length != sr.length) names.pop();
+		if(names.length === 0 || sr.length === 0) return -1;
+		*/
+
+		let data = readData();
+		let names = data.names;
+		let sr = data.sr;
+		const teamsize = data.teamsize;
+		/* Add messages to user */
+
 		const pool = sr.length;
 		let teamSR = 0, choice, arr = [], poolSize;
 		poolSize = Math.floor(names.length/teamsize);
 		let teams = []//Array(poolSize).fill(new Array());
+
+		/* Merge Lists */
+
 		for(let i = 0; i < pool; i++){
 			let name = names.pop();
 			let rank = sr.pop();
@@ -112,7 +164,9 @@ $(document).ready(function(){
 		for(let i = 0; i<teamsize; i++){
 			pools.push(arr.splice(0,Math.min(arr.length, poolSize)))
 		}
+
 		/* Divide in to teams */
+
 		for(let i = 0; i<poolSize; i++){
 			teams.push([]);
 			for(let j = 1; j < teamsize-1; j++){
@@ -125,7 +179,9 @@ $(document).ready(function(){
 											{return member.rank+totalRanking},0)/teamsize);
 			teamSR += teams[i].avgSr;
 		}
+
 		/*Calculate interesting stats */
+
 		teamSR = Math.floor(teamSR/poolSize);
 		teams.sort((a,b) => {
 			if(a.avgSr < b.avgSr)return -1;
@@ -133,7 +189,12 @@ $(document).ready(function(){
 			return 0;
 		});
 		let diff = teams[poolSize-1].avgSr-teams[0].avgSr
-		let teamOutput = "Largest Difference: " + diff + "\nAverage Sr: " + teamSR + "\nTeams:\n";
+
+		/* Output */
+
+		let teamOutput = "Largest Difference: " + diff
+		teamOutput += "\nAverage Sr: " + teamSR + "\nTeams:\n";
+		teamOutput += (arr.length === 0) ? "" : "Excluded: " + arr.map(player => player.name).join(", ") + "\n"
 		for(i = 0; i < teams.length; i++){
 			teamOutput += "Team " + (i+1) + " Average Sr: " + teams[i].avgSr;
 			for(j = 0; j < teams[i].length-1; j++){
@@ -141,7 +202,6 @@ $(document).ready(function(){
 			}
 			teamOutput += "\n"
 		}
-
 		$("#teamDisplay").html(teamOutput);
 	})
 });
